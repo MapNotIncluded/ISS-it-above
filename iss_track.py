6d1469c5219cdeb2c302f0a69761a36750d200d7
Jason Anderson <211980267+MapNotIncluded@users.noreply.github.com>
2025-05-20 16:59:51 +0200

    Bug fixes and house keeping on some code

--- iss_track.py
+++ iss_track.py
@@ -17,10 +17,6 @@ SUNRISE_SUNSET_API = os.environ.get('SUNRISE_SUNSET_API')
 CLOUD_COVER_API = os.environ.get('CLOUD_COVER_API')
 LOCATION_API = os.environ.get('GEOLOCATION_API')
 
-# My Data
-MY_LAT = float(os.environ.get('MY_LAT'))
-MY_LONG = float(os.environ.get('MY_LONG'))
-
 # Environmental Data
 ERROR = 5
 CLOUD_COVER_THRESHOLD = 50
@@ -31,8 +27,6 @@ CHECK_INTERVAL = 60
 MY_EMAIL = os.environ.get('MY_EMAIL')
 APP_PASSWORD = os.environ.get('APP_PASSWORD')
 
-
-
 # Global variable
 user_list = []
 iss_latitude = None
@@ -41,7 +35,6 @@ cloud_coverage = None
 data_report = []
 num_of_checks = 0
 num_of_passes = 0
-
 
 
 """Writes and update a report in csv format"""
@@ -74,12 +67,12 @@ def write_report(report_message):
     dataframe = pandas.DataFrame(data_report)
     dataframe.to_csv("./ISS_Report.csv")
 
-
-"""Attempts to connect to a specified API to access data
-Returns data in JSON format
+"""Attempts to connect to a specified API to retrieve relevant data
+Returns api data in JSON format
 If Connection Error/Timeout exception write a report, wait for random period of time,
-and try to reattempt connection up to max retries"""
+and try to reattempt connection up to max number of retries"""
 def connect_to_api(api, api_name, max_retries=15, **kwargs):
+    # Try multiple attempts at connecting to api
     for attempt in range(max_retries):
         try:
             response = requests.get(url=api, **kwargs)
@@ -87,11 +80,11 @@ def connect_to_api(api, api_name, max_retries=15, **kwargs):
             data = response.json()
         except ConnectionError:
             write_report(f"{api_name} Connection Error")
-            sleep(random.randint(1, 30))
+            sleep(random.randint(1, 70))
             continue
         except requests.exceptions.ConnectTimeout:
             write_report(f"{api_name} Connection Timeout")
-            sleep(random.randint(1, 40))
+            sleep(random.randint(1, 60))
             continue
         else:
             response.close()
@@ -101,9 +94,97 @@ def connect_to_api(api, api_name, max_retries=15, **kwargs):
     write_report(f"{api_name} failed after {max_retries} attempts.")
     return None
 
-"""Check if ISS is above (within a margin of error)
-Returns True if ISS above, and False if not
-Exception write a report and wait for random period of time before trying again"""
+"""Uses the users address to retrieve there latitude and longitude coordinates.
+Returns a tuple in form: (latitude, longitude)."""
+def get_user_coordinates(user_address):
+    # Geoapify API parameters
+    geolocation_param = {
+        "apiKey": os.environ.get('GEOAPIFY_KEY'),
+        "text": user_address,
+        "format": "json"
+    }
+
+    # Get users latitude and longitude
+    location_data = connect_to_api(api=LOCATION_API, api_name="Location Data", params=geolocation_param)
+
+    # Check if location_data exceeds maximum retries or returns an invalid dict format
+    if location_data is None or not location_data.get("results"):
+        write_report(f"Location data retrieval failed for address: {user_address}")
+        return None
+
+    # Attempt to format the latitude and longitude data
+    try:
+        user_lat = float(location_data["results"][0]["lat"])
+        user_long = float(location_data["results"][0]["lon"])
+    # If exception, write report and return None for coordinates
+    except (KeyError, IndexError, TypeError) as error:
+        write_report(f"Geoapify API format error: {error}")
+        return None
+
+    # Reformat latitude and longitude as tupple coordinates
+    user_location = (user_lat, user_long)
+    return user_location
+
+"""Reads user data from file, reformats data and generates user coordinates."""
+def retrieve_user_data():
+    # Attempt to read users in from json file
+    try:
+        with open("users.json", "r") as user_file:
+            all_users = json.load(user_file)
+
+    # If file not found, write report and exit app
+    except FileNotFoundError:
+        write_report("users.json not found.")
+        print("ERROR: users.json file not found.")
+        exit(1)
+
+    # If decode error, write report and exit app
+    except json.JSONDecodeError as the_error:
+        write_report(f"users.json format error: {the_error}")
+        print(f"ERROR: Invalid JSON in users.json: {the_error}")
+        exit(1)
+
+    # For each user retrieved from json.file
+    for the_user in all_users:
+
+        # Break address data into smaller parts delimited by ','
+        address_data = the_user["Address"].split(",")
+        # Remove whitespace from parts of address
+        address_data = [part.strip() for part in address_data]
+
+        # Validate user address data [suburb, city, country]
+        if not len(address_data) == 3:
+            write_report(f"Invalid Address: {the_user["Name"]}: {the_user["Address"]}")
+            print(f"Invalid Address: {the_user["Name"]}: {the_user["Address"]}")
+            # Skip over user from file and move to next one
+            continue
+
+        # Get suburb and city data from address parts
+        suburb = address_data[0]
+        city = address_data[1]
+        location_name = f"{suburb}, {city}"
+
+        # Validate users addresses result in valid coordinates
+        coordinates = get_user_coordinates(the_user["Address"])
+
+        if coordinates is None:
+            write_report(f"Could not retrieve coordinates for {the_user["Name"]} - {the_user["Address"]}")
+            # Skip over user from file and move to next one
+            continue
+
+        # Create a user profile
+        user_profile = {
+            "Name": the_user["Name"],
+            "Email": the_user["Email"],
+            "Coordinates": coordinates,
+            "Location Name": location_name
+        }
+
+        # Add to programs user list
+        user_list.append(user_profile)
+
+"""Check if ISS is above (within a defined margin of error) of users latitude and longitude.
+Returns True if ISS above, and False if not."""
 def check_iss_overhead(user_coordinates):
     # Get ISS location Data
     global iss_latitude, iss_longitude
@@ -114,12 +195,23 @@ def check_iss_overhead(user_coordinates):
 
     # Get current ISS location and reformat it
     iss_data = connect_to_api(api=ISS_API, api_name="ISS API", max_retries=10)
-    iss_latitude = float(iss_data["iss_position"]["latitude"])
-    iss_longitude = float(iss_data["iss_position"]["longitude"])
 
-    # Print current ISS lat and long to screen
-    print(f"lat: {iss_latitude}")
-    print(f"long: {iss_longitude}")
+    # Check if iss_data api call exceeds maximum retries
+    if iss_data is None:
+        write_report("ISS API returned no data")
+        return False
+
+    # Try to format retrieved iss_data
+    try:
+        iss_latitude = float(iss_data["iss_position"]["latitude"])
+        iss_longitude = float(iss_data["iss_position"]["longitude"])
+        # Print current ISS lat and long to screen
+        print(f"lat: {iss_latitude}")
+        print(f"long: {iss_longitude}")
+    # If exceptions, write report and return False (ISS not above)
+    except (KeyError, TypeError, ValueError) as error:
+        write_report(f"ISS API Format Error: {error}")
+        return False
 
     # Check if ISS is within ERROR margin of users latitude and longitude
     if (lat - ERROR) <= iss_latitude <= (lat + ERROR) and (long - ERROR) <= iss_longitude <= (long + ERROR):
@@ -127,8 +219,7 @@ def check_iss_overhead(user_coordinates):
     else:
         return False
 
-
-"""Checks if the sky is currently dark at my latitude and longitude
+"""Checks if the sky is currently dark at the users latitude and longitude.
 Returns True if it is dark, False if not"""
 def check_if_dark(user_coordinates):
     # Retrieve lat and long from user coordinates
@@ -144,10 +235,22 @@ def check_if_dark(user_coordinates):
 
     # Get sunlight data and reformat sunrise and sunset hour
     sun_data = connect_to_api(api=SUNRISE_SUNSET_API, api_name="Sunlight API", params=light_data_parameters)
-    sunrise_hour = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
-    sunset_hour = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
 
-    # Get the time now:
+    # Check if sunlight data api call has exceeded maximum retries
+    if sun_data is None:
+        write_report("Sunlight API returned no data")
+        return False
+
+    # Attempt to format retrieved sunlight data
+    try:
+        sunrise_hour = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
+        sunset_hour = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
+    # If exceptions, write report and return false (not dark outside)
+    except (KeyError, IndexError, ValueError) as error:
+        write_report(f"Sunlight API format error: {error}")
+        return False
+
+    # Get the current time:
     current_time = datetime.now()
     current_hour = current_time.hour
 
@@ -157,11 +260,9 @@ def check_if_dark(user_coordinates):
     else:
         return False
 
-
-"""Retrieve the cloud cover for my latitude and longitude
-and returns True if less than CLOUD COVER THRESHOLD
-If exception write a report and wait for random period of time before trying again"""
-def check_not_cloudy(user_coordinates):
+"""Retrieve the cloud cover percentage for users latitude and longitude.
+Returns True if cloud cover less than CLOUD_COVER_THRESHOLD, or False otherwise."""
+def check_cloud_coverage(user_coordinates):
     global cloud_coverage
 
     # Retrieve lat and long from user coordinates
@@ -177,7 +278,19 @@ def check_not_cloudy(user_coordinates):
 
     # Retrieve cloud coverage data, and reformat data
     cloud_data = connect_to_api(api=CLOUD_COVER_API, api_name="Cloud Cover API", params=cloud_parameters)
-    cloud_coverage = cloud_data["current"]["cloud_cover"]
+
+    # Check if cloud cover api calls exceeded maximum retries
+    if cloud_data is None:
+        write_report("Cloud API returned no data")
+        return False
+
+    # Attempt to format retrieved cloud cover data
+    try:
+        cloud_coverage = cloud_data["current"]["cloud_cover"]
+    # If exception, write report and return false (clear skies)
+    except (KeyError, TypeError) as error:
+        write_report(f"Cloud Cover API format error: {error}")
+        return False
 
     # Check if cloud cover is below cloud cover threshold
     if cloud_coverage <= CLOUD_COVER_THRESHOLD:
@@ -186,68 +299,21 @@ def check_not_cloudy(user_coordinates):
         return False
 
 
-def get_user_coordinates(user_address):
-    # Geoapify API parameters
-    geolocation_param = {
-        "apiKey": os.environ.get('GEOAPIFY_KEY'),
-        "text": user_address,
-        "format": "json"
-    }
-
-    # Get users latitude and longitude
-    location_data = connect_to_api(api=LOCATION_API, api_name="Location Data", params=geolocation_param)
-    user_lat = location_data["results"][0]["lat"]
-    user_long = location_data["results"][0]["lon"]
-
-    # Reformat latitude and longitude coordinates
-    user_location = (user_lat, user_long)
-    return user_location
-
-
-def retrieve_user_data():
-    # Extract user information from file
-    with open("users.json", "r") as user_file:
-        all_users = json.load(user_file)
-
-        # For each user retrieved from json.file
-        for the_user in all_users:
-
-            # Break address data into smaller parts delimited by ','
-            address_data = the_user["Address"].split(",")
-            # Remove whitespace from parts of address
-            address_data = [part.strip() for part in address_data]
-
-            # Validate user address data
-            if not len(address_data) == 3:
-                write_report(f"Invalid Address: {the_user["Name"]}: {the_user["Address"]}")
-                print(f"Invalid Address: {the_user["Name"]}: {the_user["Address"]}")
-                # Skip over user from file and move to next one
-                continue
-
-            # Get suburb and city data from address parts
-            suburb = address_data[0]
-            city = address_data[1]
-            location_name = f"{suburb}, {city}"
-
-            # Create a user profile
-            user_profile = {
-                "Name": the_user["Name"],
-                "Email": the_user["Email"],
-                "Coordinates": get_user_coordinates(the_user["Address"]),
-                "Location Name": location_name
-            }
-
-            # Add to programs user list
-            user_list.append(user_profile)
-
-
-
 # Retrieve and format user data from file
 retrieve_user_data()
 
-# Write report if successful
-write_report("User data imported successfully from file")
-print("User data successfully imported from file.\n")
+num_of_users = len(user_list)
+
+# Report and display the number of users that are successfully imported
+if num_of_users > 0:
+    write_report(f"{len(user_list)} users data imported successfully from file")
+    print(f"Users imported: {len(user_list)}")
+
+# Otherwise write report, display error message and exit program
+else:
+    write_report(f"No user data has been imported. Exiting program...")
+    print(f"No user data could be imported. Exiting program...")
+    exit(1)
 
 # Continuously run every 30 seconds, until the ISS has gone overhead 3 times
 # Check that the ISS above, that it is dark and low cloud cover
@@ -255,56 +321,87 @@ print("User data successfully imported from file.\n")
 # write report, pause for ISS to pass over, and increase number of ISS passes
 write_report("Process START")
 print("Process Starting...\n")
-while num_of_passes <= NUMBER_OF_ISS_PASSES:
+while num_of_users > 0 and num_of_passes <= NUMBER_OF_ISS_PASSES:
+
+    # Check each user in the user list
     for user in user_list:
+        # Timeout between checks
         sleep(CHECK_INTERVAL)
+
+        # Index for reporting
         num_of_checks += 1
+
+        # Check if the ISS above, if its dark and if there is low cloud coverage
         iss_above = check_iss_overhead(user["Coordinates"])
         is_dark = check_if_dark(user["Coordinates"])
-        clear_sky = check_not_cloudy(user["Coordinates"])
-        if iss_above and is_dark and clear_sky:
-            with smtplib.SMTP("smtp.gmail.com") as connection:
-                connection.starttls()
-                connection.login(user=MY_EMAIL, password=APP_PASSWORD)
-                connection.sendmail(
-                    from_addr=MY_EMAIL,
-                    to_addrs=user["Email"],
-                    msg="Subject: The ISS is Above\n\n"
-                        f"The International Space Station is flying above {user["Location Name"]} right now!\n"
-                        f"Look up!\n"
-                        f"Current ISS Latitude: {iss_latitude}\n"
-                        f"Current ISS Longitude: {iss_longitude}\n"
-                        f"\nClear Skies!\n"
-                        f"With love,\n"
-                        f"Jay")
+        clear_sky = check_cloud_coverage(user["Coordinates"])
+
+        # If the ISS is above, cloud coverage lower than threshold and it is dark outside
+        if iss_above and clear_sky and is_dark:
+
+            # Attempt to email the user to notify them
+            try:
+                # Open secure email connection and write email to notify users
+                with smtplib.SMTP("smtp.gmail.com") as connection:
+                    connection.starttls()
+                    connection.login(user=MY_EMAIL, password=APP_PASSWORD)
+                    connection.sendmail(
+                        from_addr=MY_EMAIL,
+                        to_addrs=user["Email"],
+                        msg="Subject: The ISS is Above\n\n"
+                            f"The International Space Station is flying above {user["Location Name"]} right now!\n"
+                            f"Look up!\n"
+                            f"Current ISS Latitude: {iss_latitude}\n"
+                            f"Current ISS Longitude: {iss_longitude}\n"
+                            f"\nClear Skies!\n"
+                            f"With love,\n"
+                            f"Jay")
+
+            # If exception caught write report and continue to next user
+            except Exception as error:
+                write_report(f"Email failed to send to {user["Email"]}: {error}.")
+                # Skip current user
+                continue
+
+            # Increase the number of ISS passes
             num_of_passes+= 1
+
+            # Write report of ISS pass in good conditions
             print("\nSuccess: Notification sent\n")
             write_report("Success: Notification sent")
+
             # Timeout program to ensure no duplicate notifications
             sleep(600)
+
+        # if only the ISS passes above, write a report
         elif iss_above:
             write_report("ISS passed overhead")
             print(f"ISS Above: {iss_above}, Dark: {is_dark}, Clear Sky: {clear_sky}")
-            sleep(240)
+
+            # Timeout for 5min
+            sleep(300)
+
+        # Otherwise print to screen current conditions
         else:
             print(f"ISS Overhead: {iss_above}")
             print(f"Dark Outside: {is_dark}")
             print(f"Clear Skies:  {clear_sky}")
 
 
-
 # Write final report
 write_report("Process END")
 print("\nEnd Successful")
 
-#TODO add feature to run all users in different locations parallel
+#TODO add feature to run all users in different locations in parallel
 
 #TODO add feature to add additional users to file
 
-#TODO Make getting location data more efficient
-# (i.e only get location data for person the very first time then store to encrypted file)
+#TODO ensure users do not get spam emailed, consider adding notification time stamps to user file
 
 #TODO Create a simple encryption for user data written to and read from json file/Store data in encrypted form
+
+#TODO Make getting location data more efficient
+# (i.e only get location data for person the very first time then store to encrypted file)
 
 #TODO Make get requests more robust to avoid timeout/connection errors
 
